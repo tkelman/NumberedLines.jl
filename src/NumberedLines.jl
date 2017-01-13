69180e4df17b2f2d351af6c23a86bfd45ab6fade
Brandon Taylor <brandon.taylor221@gmail.com>
2017-01-12 23:04:49 -0500

    added tests

--- src/NumberedLines.jl
+++ src/NumberedLines.jl
@@ -37,6 +37,8 @@ is_line_number(1)
     immutable NumberedLine
 
 A line of code with an attached line number
+
+print(NumberedLine(LineNumberNode(1), 2))
 """
 immutable NumberedLine
     number
@@ -45,7 +47,7 @@ end
 
 safe_get(vector, index) =
     if index > length(vector)
-        nothing
+        error("Line number with no associated line")
     else
         vector[index]
     end
@@ -72,14 +74,17 @@ julia> e = quote
            end
        end;
 
-julia> typeof( attach_line_numbers(e).args[1] )
+julia> attached_arguments = attach_line_numbers(e).args;
+
+julia> attached_arguments[1] |> typeof
 NumberedLines.NumberedLine
 
-julia> typeof( attach_line_numbers(e).args[2] )
+julia> attached_arguments[2].line.args[1] |> typeof
 NumberedLines.NumberedLine
 
-julia> typeof( attach_line_numbers(e).args[2].line.args[1] )
-NumberedLines.NumberedLine
+julia> no_line = Expr(:block, LineNumberNode(1) );
+
+julia> Test.@test_throws ErrorException attach_line_numbers(no_line);
 ```
 """
 attach_line_numbers(a) = a
@@ -119,15 +124,18 @@ julia> e = quote
            1
        end;
 
+julia> push!(e.args, 2);
+
 julia> e |> attach_line_numbers |> detach_line_numbers == e
 true
+
+julia> detach_line_numbers(1)
+1
 ```
 """
 detach_line_numbers(a) = a
 detach_line_numbers(e::Expr) = Expr(e.head, vcat(map(separate, e.args)...)...)
-detach_line_numbers(n::NumberedLine) = Expr(:block, n.number, n.line)
-
-Base.string(n::NumberedLine) = string(detach_line_numbers(n) )
+detach_line_numbers(n::NumberedLine) =
 
 """
     without_line_number(f, n::NumberedLine)
@@ -147,6 +155,9 @@ julia> test_line = attach_line_numbers(e).args[1];
 
 julia> without_line_number(add_one, test_line) ==
            NumberedLine(test_line.number, add_one(test_line.line) )
+true
+
+julia> without_line_number(add_one, 1) == add_one(1)
 true
 ```
 """
